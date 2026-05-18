cbe5edf2594b5a48f593372b7f8caa4c65931ff9
ytun9959-design <ytun9959@gmail.com>
2026-05-18 11:19:02 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ def run_web_server():
 BOT_TOKEN = "8843922979:AAFZLgAekpj-QMblNTTVPWdHNs4kbPPoMDA"
 ADMIN_ID = 8701781484
 
-GITHUB_TOKEN = os.getenv("GH_TOKEN") if os.getenv("GH_TOKEN") else "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"
+GITHUB_TOKEN = os.getenv("GH_TOKEN") if os.getenv("GH_TOKEN") else "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"
 REPO_OWNER = "ytun9959-design" 
 REPO_NAME = "Auth" 
 FILE_PATH = "key.txt" 
